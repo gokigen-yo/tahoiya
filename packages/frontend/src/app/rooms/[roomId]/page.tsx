da417c08f6a785ea4eec853fafafbcbd2a3f0c72
gokigen-yo <155721102+gokigen-yo@users.noreply.github.com>
2026-02-01 11:33:23 +0900

    Implement join room page (#26)

--- packages/frontend/src/app/rooms/[roomId]/page.tsx
+++ packages/frontend/src/app/rooms/[roomId]/page.tsx
@@ -1,10 +1,7 @@
+import { RoomAuthGuard } from "@/features/room/containers/RoomAuthGuard";
+
 export default async function RoomPage({ params }: { params: Promise<{ roomId: string }> }) {
   const { roomId } = await params;
 
-  return (
-    <div>
-      <h1>Room: {roomId}</h1>
-      {/* TODO: Implement RoomContainer here */}
-    </div>
-  );
+  return <RoomAuthGuard roomId={roomId} />;
 }
